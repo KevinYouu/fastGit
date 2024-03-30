02555f8910eb8c79db0a14688c5ae56275990b1a
KevinYouu <zdy295@gmail.com>
2024-03-30 17:47:23 +0800

    feat 提交部分修改文件

--- git-functions/push/selected.go
+++ git-functions/push/selected.go
@@ -5,7 +5,9 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/KevinYouu/fastGit/functions/choose"
 	"github.com/KevinYouu/fastGit/functions/colors"
+	"github.com/KevinYouu/fastGit/functions/input"
 	"github.com/KevinYouu/fastGit/functions/multipleChoice"
 	"github.com/KevinYouu/fastGit/git-functions/status"
 )
@@ -26,7 +28,15 @@ func PushSelected() {
 
 	data := multipleChoice.MultipleChoice(selectedFiles)
 
-	cmd := exec.Command("git", append([]string{"add"}, data...)...)
+	cmd := exec.Command("git", "pull")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error executing git pull command:", err)
+	} else {
+		fmt.Println("Pulled successfully.")
+	}
+
+	cmd = exec.Command("git", append([]string{"add"}, data...)...)
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println("Error executing git add command:", err)
@@ -34,4 +44,25 @@ func PushSelected() {
 	}
 
 	fmt.Println("Files added successfully.")
+	suffix := choose.Choose([]string{"fix", "feat", "docs", "style", "refactor", "test", "chore", "revert"})
+	commitMessage := input.Input("Enter your commit message: \n", "commit message", "\n(esc to quit)")
+
+	cmd = exec.Command("git", "commit", "-m", suffix+" "+commitMessage)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error executing git commit command:", err)
+		return
+	}
+
+	fmt.Println("Commit successful.")
+
+	// 执行 git push 命令
+	cmd = exec.Command("git", "push")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error executing git push command:", err)
+		return
+	}
+
+	fmt.Println("Push successful.")
 }

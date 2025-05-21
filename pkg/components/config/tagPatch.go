731ab2c80ed0eab9d9bc81392a665aec7af4381c
KevinYou <zdy295@gmail.com>
2025-05-21 11:26:59 +0800

    fix: enhance error handling in GetTagPatch by adding initialization step

--- pkg/components/config/tagPatch.go
+++ pkg/components/config/tagPatch.go
@@ -18,13 +18,18 @@ func GetTagPatch() (Patch, error) {
 	defer db.Close()
 
 	row := db.QueryRow("SELECT prefix, major, minor, patch, suffix FROM patches LIMIT 1")
-
 	var patch Patch
 	if err := row.Scan(&patch.Prefix, &patch.Major, &patch.Minor, &patch.Patch, &patch.Suffix); err != nil {
-		if err == sql.ErrNoRows {
+		if initErr := Initialize(); initErr != nil {
+			return GetDefaultTagPatch()[0], initErr
+		}
+		row = db.QueryRow("SELECT prefix, major, minor, patch, suffix FROM patches LIMIT 1")
+		if err := row.Scan(&patch.Prefix, &patch.Major, &patch.Minor, &patch.Patch, &patch.Suffix); err != nil {
+			if err == sql.ErrNoRows {
+				return GetDefaultTagPatch()[0], err
+			}
 			return GetDefaultTagPatch()[0], err
 		}
-		return GetDefaultTagPatch()[0], err
 	}
 
 	return patch, nil
